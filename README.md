af675acb205a1bbc7df693791f1d789275c306c6
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2021-12-06 10:00:06 -0300

    @robertwtm Alterando a senhado do banco de dados

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Atividades desenvolvida no bloco 2 para a construção de um blog
 
 [- Atividade CRUD Back-end blog-pessoal 1.](https://drive.google.com/file/d/1ksxO2xzCwOuA7WSTLpmNq966qa7dDSO5/view)
 
+[- Atividade CRUD Back-end blog-pessoal 2.](https://drive.google.com/file/d/1Rtnm-dpIu4TNVQHZBypcHnIWzKulFhxh/view)
+
+[- Atividade CRUD Back-end blog-pessoal 2.](https://drive.google.com/file/d/1vvsc0t-EUZGEtPEwTwT9XZNkoo2uGWLp/view)
--- src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,30 +6,37 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "postagem")
 public class Postagem {
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String titulo;
-	
+
 	@NotNull
 	@Size(min = 10, max = 500)
 	private String texto;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -63,4 +70,11 @@ public class Postagem {
 		this.date = date;
 	}
 
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 }
