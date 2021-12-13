518b88316a75a1f967e3f8bd53840207d63fa8b0
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2021-12-13 20:35:47 -0300

    Security

--- src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -3,10 +3,15 @@ package org.generation.blogPessoal.controller;
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
 import org.generation.blogPessoal.service.UsuarioService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
--- src/main/java/org/generation/blogPessoal/security/UserDetailsImpl.java
+++ src/main/java/org/generation/blogPessoal/security/UserDetailsImpl.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 public class UserDetailsImpl implements UserDetails {
 
@@ -12,6 +13,7 @@ public class UserDetailsImpl implements UserDetails {
 
     private String userName;
     private String password;
+    private List<GrantedAuthority> authorities;
 
     public UserDetailsImpl(Usuario user) {
         this.userName = user.getUsuario();
