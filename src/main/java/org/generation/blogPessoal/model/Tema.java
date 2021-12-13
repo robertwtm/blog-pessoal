7708512cbc6412b5ba019f7f613936f9d04e6aed
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2021-12-13 09:45:41 -0300

    Update Model

--- src/main/java/org/generation/blogPessoal/model/Tema.java
+++ src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -9,19 +9,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table(name = "tb_tema")
+@Table(name = "tbTema")
 public class Tema {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
+	@NotBlank
 	private String descricao;
 	
 	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
