06bcf2203bfcecfa5ae96b31a763f147259d2c5a
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2022-01-27 11:33:25 -0300

    Update BasicSecurityConfig.java

--- src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -39,11 +39,12 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	 @Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers("/**").permitAll()
 			.antMatchers(HttpMethod.POST, "/usuarios/logar").permitAll()
 			.antMatchers(HttpMethod.POST, "/usuarios/cadastrar").permitAll()
-			.antMatchers(HttpMethod.OPTIONS).permitAll()
 			.antMatchers(HttpMethod.GET ,"/postagens").permitAll()
-			.antMatchers(HttpMethod.GET ,"/tema").permitAll()
+			.antMatchers(HttpMethod.GET ,"/temas").permitAll()
+			 .antMatchers(HttpMethod.OPTIONS).permitAll()
 			.anyRequest().authenticated()
 			.and().httpBasic()
 			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
